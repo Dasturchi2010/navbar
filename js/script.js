1dfdaacd867f1826e9dc9d463be16501493eae83
Diyorbek <johndoe@example.com>
2024-07-22 12:38:31 +0500

    navbar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let btns = document.querySelectorAll(".btn")
+let navbar = document.querySelector(".navbar")
+
+
+
+btns.forEach((btn)=> {
+    btn.addEventListener("click", ()=> {
+        navbar.style.background = btn.getAttribute("data-color")
+    })
+})
